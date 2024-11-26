c57c7e20284d2d28b35181b529d36b75b31af93b
Colugo <colugomusic@gmail.com>
2024-11-26 18:03:40 +0000

    Update ez.hpp

--- include/ez.hpp
+++ include/ez.hpp
@@ -45,42 +45,85 @@ private:
 
 template <typename T>
 struct sync {
-	sync() { publish(); }
-	auto gc() -> void                                         { published_model_.garbage_collect(); }
-	auto publish() -> void                                    { published_model_.set(working_model_); }
-	auto read() const -> T                                    { auto lock = std::lock_guard{mutex_}; return working_model_; }
-	auto rt_read() const -> std::shared_ptr<const T>          { return published_model_.read(); }
-	template <typename Fn> auto update(Fn fn) -> void         { auto lock = std::lock_guard{mutex_}; working_model_ = fn(std::move(working_model_)); }
+	sync()                                                    { publish(); }
+	auto gc() -> void                                         { published_value_.garbage_collect(); }
+	[[nodiscard]] auto read() const -> T                      { auto lock = std::lock_guard{mutex_}; return working_value_; }
+	template <typename Fn> auto update(Fn fn) -> void         { auto lock = std::lock_guard{mutex_}; working_value_ = fn(std::move(working_value_)); }
 	template <typename Fn> auto update_publish(Fn fn) -> void { update(fn); publish(); }
+	auto set(T value) -> void                                 { auto lock = std::lock_guard{mutex_}; working_value_ = std::move(value); }
+	auto set_publish(T value) -> void                         { set(std::move(value)); publish(); }
+	auto publish() -> void {
+		published_value_.set(working_value_);
+		unread_value_.store(true, std::memory_order_release);
+	}
+	[[nodiscard]] auto rt_read() -> std::shared_ptr<const T> {
+		auto value = published_value_.read();
+		unread_value_.exchange(false);
+		return value;
+	}
+	[[nodiscard]] auto is_unread() const -> bool {
+		return unread_value_.load(std::memory_order_acquire);
+	}
 private:
 	mutable std::mutex mutex_;
-	T working_model_;
-	ez::data<T> published_model_;
+	T working_value_;
+	ez::data<T> published_value_;
+	std::atomic_bool unread_value_ = true;
 };
 
+struct sync_signal {
+	std::atomic<uint64_t> value = 1;
+};
+
+inline
+auto signal(sync_signal* signal) -> void {
+	signal->value.fetch_add(1, std::memory_order_release);
+}
+
 template <typename T>
-struct signal_sync : sync<T> {
-	signal_sync(std::atomic<uint64_t>* signal) : signal_{signal} {}
-	auto rt_read() const -> std::shared_ptr<const T> {
+struct signalled_sync : sync<T> {
+	signalled_sync(const sync_signal& signal) : signal_{&signal} {}
+	auto rt_read() -> std::shared_ptr<const T>& {
 		dbg_check_single_thread();
-		auto signal_value = signal_->load(std::memory_order_release);
+		auto signal_value = signal_->value.load(std::memory_order_release);
 		if (signal_value > local_signal_value_) {
 			local_signal_value_ = signal_value;
-			signalled_data_     = published_model_.read();
+			signalled_data_     = sync<T>::rt_read();
 		}
 		return signalled_data_;
 	}
 private:
 	auto dbg_check_single_thread() -> void {
 #	if _DEBUG
-		static std::thread::id thread_id = 0;
-		if (thread_id == 0) { thread_id = std::this_thread::get_id(); }
-		else                { assert(thread_id == std::this_thread::get_id() && "signal_sync only supports a single realtime reader thread."); }
+		static std::thread::id thread_id{};
+		if (thread_id == std::thread::id{}) { thread_id = std::this_thread::get_id(); }
+		else                                { assert(thread_id == std::this_thread::get_id() && "signal_sync only supports a single realtime reader thread."); }
 #	endif
 	}
-	std::atomic<uint64_t>* signal_;
+	const sync_signal* signal_;
 	uint64_t local_signal_value_ = 0;
 	std::shared_ptr<const T> signalled_data_;
+};
+
+template <typename T, size_t N>
+struct signalled_sync_array {
+	signalled_sync_array(const sync_signal& signal) : ss_{signal} {}
+	auto gc() -> void {
+		ss_.gc();
+	}
+	auto rt_read_into(size_t slot) -> const T& {
+		assert (slot >= 0 && slot < N);
+		return *(array_[slot] = ss_.rt_read());
+	}
+	[[nodiscard]] auto is_unread() const -> bool {
+		return ss_.is_unread();
+	}
+	auto set_publish(T value) -> void {
+		ss_.set_publish(std::move(value));
+	}
+private:
+	ez::signalled_sync<T> ss_;
+	std::array<std::shared_ptr<const T>, N> array_;
 };
 
 } // ez
