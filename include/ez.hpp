9ad59853f336ea005358513b9d9a61104b3c14fa
Colugo <colugomusic@gmail.com>
2024-11-28 13:13:08 +0000

    Update ez.hpp

--- include/ez.hpp
+++ include/ez.hpp
@@ -256,12 +256,4 @@ private:
 	std::array<immutable<T>, N> array_;
 };
 
-struct trigger {
-	trigger()                 { flag_.clear(); flag_.test_and_set(std::memory_order_relaxed); }
-	auto operator()() -> void { flag_.clear(std::memory_order_relaxed); }
-	operator bool()           { return !(flag_.test_and_set(std::memory_order_relaxed)); }
-private:
-	std::atomic_flag flag_;
-};
-
 } // ez
