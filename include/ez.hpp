b93273b1f3fa9868d2b6bed4d87c2154968d1f6c
Colugo <colugomusic@gmail.com>
2025-07-02 10:51:22 +0100

    Update ez.hpp

--- include/ez.hpp
+++ include/ez.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "ez-tags.hpp"
 #include <cassert>
 #include <deque>
 #include <memory>
@@ -8,38 +9,6 @@
 #include <vector>
 
 namespace ez {
-
-// These tags have no runtime cost. The point of them is just to force the user
-// to type ez::rt or ez::nort to reduce the chance of accidentally calling a
-// non-realtime-safe function from a realtime thread.
-
-struct nort_t {}; // Indicates that the calling thread is not a realtime thread.
-struct rt_t {};   // Indicates that the calling thread is a realtime thread.
-
-// Used to indicate that a function is completely thread-safe and realtime-safe.
-struct safe_t {
-	safe_t() = default;
-	safe_t(nort_t){}
-	safe_t(rt_t){}
-};
-
-static constexpr auto nort = nort_t{};
-static constexpr auto rt   = rt_t{};
-static constexpr auto safe = safe_t{};
-
-// Just some aliases.
-using audio_t = rt_t;
-using gc_t    = nort_t;
-using main_t  = nort_t;
-using ui_t    = nort_t;
-static constexpr auto audio = rt;
-static constexpr auto gc    = nort;
-static constexpr auto main  = nort;
-static constexpr auto ui    = nort;
-
-// It is possible for the user to lie about whether they are calling a function
-// from a realtime thread or not. If they do that then I don't guarantee that
-// anything will work the way they expect.
 
 namespace detail {
 
@@ -169,7 +138,7 @@ template <typename T, bool auto_gc = false>
 struct sync {
 	sync()                                                             { publish(ez::nort); }
 	[[nodiscard]] auto read(ez::nort_t) const -> T                     { auto lock = std::lock_guard{mutex_}; return working_value_; }
-	[[nodiscard]] auto read(detail::published_t) -> immutable<T>       { return published_value_.read(ez::safe); }
+	[[nodiscard]] auto read(detail::published_t) const -> immutable<T> { return published_value_.read(ez::safe); }
 	auto gc(ez::gc_t) -> void                                          { published_value_.garbage_collect(ez::gc); }
 	auto publish(ez::nort_t) -> void                                   { published_value_.set(ez::nort, working_value_); }
 	auto set(ez::nort_t, T value) -> void                              { auto lock = std::lock_guard{mutex_}; working_value_ = std::move(value); }
